165a80b6701c05fdf1dc525ce37eaf89edaeec39
maxcolliander <129760842+maxcolliander@users.noreply.github.com>
2024-06-03 14:40:46 +0200

    Added folders for each os and went with Eric way

--- pickle_test.py
+++ pickle_test.py
@@ -1,7 +1,7 @@
 import unittest
 import pickle
 import platform
-
+import os
 
 class class_to_test():
     def __init__(self, x = 0, y = 0) -> None:
@@ -32,7 +32,7 @@ class TestPickle(unittest.TestCase):
     def test_pickle_string(self):
         data = 'test'
         pickled_data = pickle.dumps(data)
-        file_name = f'pickle_string_{get_os_suffix()}.pkl'
+        file_name = f'{get_os_suffix() + '_data'}\\pickle_string_{get_os_suffix()}.pkl'
         with open(file_name, 'wb') as f:
             f.write(pickled_data)
         self.assertEqual(data, pickle.loads(pickled_data))
@@ -40,7 +40,7 @@ class TestPickle(unittest.TestCase):
     def test_pickle_int(self):
         data = 1
         pickled_data = pickle.dumps(data)
-        file_name = f'pickle_int_{get_os_suffix()}.pkl'
+        file_name = f'{get_os_suffix() + '_data'}\\pickle_int_{get_os_suffix()}.pkl'
         with open(file_name, 'wb') as f:
             f.write(pickled_data)
         self.assertEqual(data, pickle.loads(pickled_data))
@@ -48,7 +48,7 @@ class TestPickle(unittest.TestCase):
     def test_pickle_class(self):
         data = class_to_test
         pickled_data = pickle.dumps(data)
-        file_name = f'pickle_class_{get_os_suffix()}.pkl'
+        file_name = f'{get_os_suffix() + '_data'}\\pickle_class_{get_os_suffix()}.pkl'
         with open(file_name, 'wb') as f:
             f.write(pickled_data)
         self.assertEqual(data, pickle.loads(pickled_data))
@@ -56,7 +56,7 @@ class TestPickle(unittest.TestCase):
     def test_pickle_instance(self):
         data = class_to_test(1, 2)
         pickled_data = pickle.dumps(data)
-        file_name = f'pickle_instance_{get_os_suffix()}.pkl'
+        file_name = f'{get_os_suffix() + '_data'}\\pickle_instance_{get_os_suffix()}.pkl'
         with open(file_name, 'wb') as f: #AssertionError: <__main__.class_to_test object at 0x000001D5F7C87B60> != <__main__.class_to_test object at 0x000001D5F76EB410>
             f.write(pickled_data)
         self.assertEqual(data, pickle.loads(pickled_data))
@@ -64,7 +64,7 @@ class TestPickle(unittest.TestCase):
     def test_pickle_dict(self):
         data = {"a": 1, "b": 2}
         pickled_data = pickle.dumps(data)
-        file_name = f'pickle_dict_{get_os_suffix()}.pkl'
+        file_name = f'{get_os_suffix() + '_data'}\\pickle_dict_{get_os_suffix()}.pkl'
         with open(file_name, 'wb') as f:
             f.write(pickled_data)
         self.assertEqual(data, pickle.loads(pickled_data))
@@ -72,7 +72,7 @@ class TestPickle(unittest.TestCase):
     def test_pickle_list(self):
         data = [1, 2, 3]
         pickled_data = pickle.dumps(data)
-        file_name = f'pickle_list_{get_os_suffix()}.pkl'
+        file_name = f'{get_os_suffix() + '_data'}\\pickle_list_{get_os_suffix()}.pkl'
         with open(file_name, 'wb') as f:
             f.write(pickled_data)
         self.assertEqual(data, pickle.loads(pickled_data))
@@ -80,14 +80,14 @@ class TestPickle(unittest.TestCase):
     def test_pickle_bool(self):
         data = True
         pickled_data = pickle.dumps(data)
-        file_name = f'pickle_bool_{get_os_suffix()}.pkl'
+        file_name = f'{get_os_suffix() + '_data'}\\pickle_bool_{get_os_suffix()}.pkl'
         with open(file_name, 'wb') as f:
             f.write(pickled_data)
     
     def test_pickle_float(self):
         data = 1.2
         pickled_data = pickle.dumps(data)
-        file_name = f'pickle_float_{get_os_suffix()}.pkl'
+        file_name = f'{get_os_suffix() + '_data'}\\pickle_float_{get_os_suffix()}.pkl'
         with open(file_name, 'wb') as f:
             f.write(pickled_data)
         self.assertEqual(data, pickle.loads(pickled_data))
@@ -95,7 +95,7 @@ class TestPickle(unittest.TestCase):
     def test_pickle_func(self):
         data = fibonacci
         pickled_data = pickle.dumps(data)
-        file_name = f'pickle_func_{get_os_suffix()}.pkl'
+        file_name = f'{get_os_suffix() + '_data'}\\pickle_func_{get_os_suffix()}.pkl'
         with open(file_name, 'wb') as f:
             f.write(pickled_data)
         self.assertEqual(data, pickle.loads(pickled_data))
@@ -103,7 +103,7 @@ class TestPickle(unittest.TestCase):
     def test_pickle_float_accuracy(self):
         data = 1/3
         pickled_data = pickle.dumps(data)
-        file_name = f'pickle_float_accuracy_{get_os_suffix()}.pkl'
+        file_name = f'{get_os_suffix() + '_data'}\\pickle_float_accuracy_{get_os_suffix()}.pkl'
         with open(file_name, 'wb') as f:
             f.write(pickled_data)
         self.assertEqual(data, pickle.loads(pickled_data))
@@ -118,4 +118,5 @@ def fibonacci(n):
 
 
 if __name__=='__main__':
+    os.makedirs(get_os_suffix() + '_data', exist_ok=True)
     unittest.main()
