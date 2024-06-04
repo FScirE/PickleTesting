06436330777d4ae2eb92beaa574fddbbe8aa3a9d
maxcolliander <129760842+maxcolliander@users.noreply.github.com>
2024-06-04 13:41:43 +0200

    opps 4

--- pyversion_test.py
+++ pyversion_test.py
@@ -4,8 +4,11 @@ import platform
 def get_python_version():
     return platform.python_version().lower()
 
+def get_os_suffix():
+    return platform.system().lower()
+
 def write_python_version_file():
-    file_name = f'{get_python_version()}.txt'
+    file_name = f'{get_os_suffix}{get_python_version()}.txt'
     with open(file_name, 'w') as f:
         f.write(get_python_version())
 
